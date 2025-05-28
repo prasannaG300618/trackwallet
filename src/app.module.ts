73c5f297d3d934aa0e10d8654c1ea0c7266c44bc
prasannaG300618 <129400945+prasannaG300618@users.noreply.github.com>
2025-05-28 23:43:35 +0530

    Update app.module.ts

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,7 +6,7 @@ import {MongooseModule}  from '@nestjs/mongoose';
 
 
 @Module({
-  imports: [AuthModule, MongooseModule.forRoot('mongodb+srv://prasannakithiyon53:Pras300618@cluster0.8fyvo.mongodb.net/trackWallet')], 
+  imports: [AuthModule, MongooseModule.forRoot('mongodb+srv://:@cluster0.8fyvo.mongodb.net/trackWallet')], 
   controllers: [AppController],
   providers:[AppService]
 })
